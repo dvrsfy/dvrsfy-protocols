acd18f92c3b2dd0846e92039975c44806b645196
Smart Contrart <smartcontrart@protonmail.com>
2024-03-30 07:08:07 -0400

    Increased test coverage

--- test/FundFactory.js
+++ test/FundFactory.js
@@ -1,97 +1,97 @@
-// const {
-//   time,
-//   loadFixture,
-// } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-// const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-// const { expect } = require("chai");
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
 
-// const constants = require("../utils/constants");
+const constants = require("../utils/constants");
 
-// const {
-//   getSigners,
-//   deployTokensFixture,
-//   deployFundFactoryFixture,
-//   deployFundFixture,
-// } = require("./Fixtures.js");
+const {
+  getSigners,
+  deployTokensFixture,
+  deployFundFactoryFixture,
+  deployFundFixture,
+} = require("./Fixtures.js");
 
-// describe("FundFactory Unit", function () {
-//   describe("Deployment", function () {
-//     it("Should deploy FundFactory", async function () {
-//       const { fundFactory } = await loadFixture(deployFundFactoryFixture);
-//       expect(fundFactory.address).to.not.equal(constants.ADDRESS_ZERO);
-//     });
+describe("FundFactory Unit", function () {
+  describe("Deployment", function () {
+    it("Should deploy FundFactory", async function () {
+      const { fundFactory } = await loadFixture(deployFundFactoryFixture);
+      expect(fundFactory.address).to.not.equal(constants.ADDRESS_ZERO);
+    });
 
-//     it("Shouldn't deploy with no pricer", async function () {
-//       const { pricer, swapper } = await loadFixture(deployFundFactoryFixture);
-//       const FundFactory = await ethers.getContractFactory("DvrsfyFundFactory");
-//       await expect(
-//         FundFactory.deploy(constants.ADDRESS_ZERO, swapper.target)
-//       ).to.be.revertedWithCustomError(FundFactory, "PricerNotSet");
-//     });
+    it("Shouldn't deploy with no pricer", async function () {
+      const { pricer, swapper } = await loadFixture(deployFundFactoryFixture);
+      const FundFactory = await ethers.getContractFactory("DvrsfyFundFactory");
+      await expect(
+        FundFactory.deploy(constants.ADDRESS_ZERO, swapper.target)
+      ).to.be.revertedWithCustomError(FundFactory, "PricerNotSet");
+    });
 
-//     it("Shouldn't deploy with no swapper", async function () {
-//       const { pricer, swapper } = await loadFixture(deployFundFactoryFixture);
-//       const FundFactory = await ethers.getContractFactory("DvrsfyFundFactory");
-//       await expect(
-//         FundFactory.deploy(pricer.target, constants.ADDRESS_ZERO)
-//       ).to.be.revertedWithCustomError(FundFactory, "SwapperNotSet");
-//     });
-//   });
+    it("Shouldn't deploy with no swapper", async function () {
+      const { pricer, swapper } = await loadFixture(deployFundFactoryFixture);
+      const FundFactory = await ethers.getContractFactory("DvrsfyFundFactory");
+      await expect(
+        FundFactory.deploy(pricer.target, constants.ADDRESS_ZERO)
+      ).to.be.revertedWithCustomError(FundFactory, "SwapperNotSet");
+    });
+  });
 
-//   describe("Create Fund", function () {
-//     it("Should create a new Fund", async function () {
-//       const { fundFactory, pricer, swapper, deployer } = await loadFixture(
-//         deployFundFactoryFixture
-//       );
+  describe("Create Fund", function () {
+    it("Should create a new Fund", async function () {
+      const { fundFactory, pricer, swapper, deployer } = await loadFixture(
+        deployFundFactoryFixture
+      );
 
-//       const { weth, pepe } = await loadFixture(deployTokensFixture);
+      const { weth, pepe } = await loadFixture(deployTokensFixture);
 
-//       const default_assets = [weth.target, pepe.target];
-//       const tx = await fundFactory.createFund(
-//         constants.DEFAULT_NAME,
-//         constants.DEFAULT_SYMBOL,
-//         constants.USDC_ADDRESS,
-//         constants.WETH_ADDRESS,
-//         constants.DEFAULT_PROTOCOL_FEE,
-//         constants.DEFAULT_MANAGEMENT_FEE
-//       );
+      const default_assets = [weth.target, pepe.target];
+      const tx = await fundFactory.createFund(
+        constants.DEFAULT_NAME,
+        constants.DEFAULT_SYMBOL,
+        constants.USDC_ADDRESS,
+        constants.WETH_ADDRESS,
+        constants.DEFAULT_PROTOCOL_FEE,
+        constants.DEFAULT_MANAGEMENT_FEE
+      );
 
-//       await expect(tx)
-//         .to.emit(fundFactory, "FundCreated")
-//         .withArgs(
-//           deployer.address,
-//           pricer,
-//           swapper,
-//           constants.DEFAULT_NAME,
-//           constants.DEFAULT_SYMBOL,
-//           constants.USDC_ADDRESS,
-//           constants.WETH_ADDRESS,
-//           constants.DEFAULT_PROTOCOL_FEE,
-//           constants.DEFAULT_MANAGEMENT_FEE
-//         );
-//     });
-//   });
+      await expect(tx)
+        .to.emit(fundFactory, "FundCreated")
+        .withArgs(
+          deployer.address,
+          pricer,
+          swapper,
+          constants.DEFAULT_NAME,
+          constants.DEFAULT_SYMBOL,
+          constants.USDC_ADDRESS,
+          constants.WETH_ADDRESS,
+          constants.DEFAULT_PROTOCOL_FEE,
+          constants.DEFAULT_MANAGEMENT_FEE
+        );
+    });
+  });
 
-//   describe("Administrative functions", function () {
-//     it("Should update the pricer", async function () {
-//       const { fundFactory } = await loadFixture(deployFundFactoryFixture);
+  describe("Administrative functions", function () {
+    it("Should update the pricer", async function () {
+      const { fundFactory } = await loadFixture(deployFundFactoryFixture);
 
-//       const { pricer } = await loadFixture(deployFundFixture);
+      const { pricer } = await loadFixture(deployFundFixture);
 
-//       const tx = await fundFactory.updatePricer(pricer.target);
-//       await expect(tx)
-//         .to.emit(fundFactory, "PricerUpdated")
-//         .withArgs(pricer.target);
-//     });
-//     it("Should update the swapper", async function () {
-//       const { fundFactory } = await loadFixture(deployFundFactoryFixture);
+      const tx = await fundFactory.updatePricer(pricer.target);
+      await expect(tx)
+        .to.emit(fundFactory, "PricerUpdated")
+        .withArgs(pricer.target);
+    });
+    it("Should update the swapper", async function () {
+      const { fundFactory } = await loadFixture(deployFundFactoryFixture);
 
-//       const { swapper } = await loadFixture(deployFundFixture);
+      const { swapper } = await loadFixture(deployFundFixture);
 
-//       const tx = await fundFactory.updateSwapper(swapper.target);
-//       await expect(tx)
-//         .to.emit(fundFactory, "SwapperUpdated")
-//         .withArgs(swapper.target);
-//     });
-//   });
-// });
+      const tx = await fundFactory.updateSwapper(swapper.target);
+      await expect(tx)
+        .to.emit(fundFactory, "SwapperUpdated")
+        .withArgs(swapper.target);
+    });
+  });
+});
--- test/Swapper.js
+++ test/Swapper.js
@@ -45,6 +45,54 @@ describe("Swapper Unit", function () {
       );
     });
 
+    it("Should revert if the swapTarget is not the 0x exchange Proxy", async function () {
+      const { swapper } = await loadFixture(deploySwapperFixture);
+      const { weth, dai, usdc } = await loadFixture(deployTokensFixture);
+      await hre.network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [constants.WHALE],
+      });
+      const whale = await ethers.getSigner(constants.WHALE);
+
+      let swapParams = await getSwapParams(
+        constants.DAI_ADDRESS,
+        constants.USDC_ADDRESS,
+        constants.TEST_SWAP
+      );
+
+      swapParams.swapTarget = constants.ADDRESS_ZERO;
+
+      await dai.connect(whale).approve(swapper.target, constants.TEST_SWAP);
+
+      await expect(swapper.connect(whale).swap(swapParams))
+        .to.be.revertedWithCustomError(swapper, "TargetInvalid")
+        .withArgs(constants.ADDRESS_ZERO);
+    });
+
+    it("Should revert if the sellToken and buyToken are identical", async function () {
+      const { swapper } = await loadFixture(deploySwapperFixture);
+      const { weth, dai, usdc } = await loadFixture(deployTokensFixture);
+      await hre.network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [constants.WHALE],
+      });
+      const whale = await ethers.getSigner(constants.WHALE);
+
+      let swapParams = await getSwapParams(
+        constants.DAI_ADDRESS,
+        constants.USDC_ADDRESS,
+        constants.TEST_SWAP
+      );
+
+      swapParams.buyToken = constants.DAI_ADDRESS;
+
+      await dai.connect(whale).approve(swapper.target, constants.TEST_SWAP);
+
+      await expect(
+        swapper.connect(whale).swap(swapParams)
+      ).to.be.revertedWithCustomError(swapper, "SwapParamsInvalid");
+    });
+
     it("Should swap in ETH", async function () {
       const { swapper } = await loadFixture(deploySwapperFixture);
       const { weth, dai, usdc } = await loadFixture(deployTokensFixture);
